e2680d84745087f675d9d67a1fb6a4bfe08ed544
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-06-16 16:06:04 +0700

    Initial Commit

---
+++ SkinPedia/SkinPediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SkinPediaApp.swift
+//  SkinPedia
+//
+//  Created by Rainer Regan on 16/06/23.
+//
+
+import SwiftUI
+
+@main
+struct SkinPediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
