4c51f6cc66ae4f49b84d5d1544afb19ab0e53dd6
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-06-20 14:36:45 +0700

    Add Gitignore

---
+++ SkinPedia/Features/SkinPedia/Presentation/Views/AnalysisResultView.swift
@@ -0,0 +1,20 @@
+//
+//  AnalysisResultView.swift
+//  SkinPedia
+//
+//  Created by Rainer Regan on 20/06/23.
+//
+
+import SwiftUI
+
+struct AnalysisResultView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnalysisResultView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnalysisResultView()
+    }
+}
