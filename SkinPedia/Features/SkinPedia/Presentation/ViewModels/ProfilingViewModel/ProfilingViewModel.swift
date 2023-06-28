bf839b797885612daa470848db3a96b984bf8177
Rainer Regan (Exacode) <rainercoc@gmail.com>
2023-06-28 15:15:12 +0700

    Create New Profiling

---
+++ SkinPedia/Features/SkinPedia/Presentation/ViewModels/ProfilingViewModel/ProfilingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  ProfilingViewModel.swift
+//  SkinPedia
+//
+//  Created by Rainer Regan on 28/06/23.
+//
+
+import Foundation
+
+class ProfilingViewModel : ObservableObject {
+    @Published var textFieldString: String = ""
+}
